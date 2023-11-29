60c6658f69abacc94af6587000704a432706c7c7
CharanKrishnan <charankovvali@gmail.com>
2023-11-29 23:08:32 +0530

    Add project files.

---
+++ AppServiceLayer/Controllers/EmpProfileApiController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AppServiceLayer.Models;
+using BussinessLogicLayer;
+using DataAccessLayer;
+
+namespace AppServiceLayer.Controllers
+{
+    public class EmpProfileApiController : ApiController
+    {
+        EmpProfileOperations profileOperations = new EmpProfileOperations();
+        public void Post(EmpProfileModel empModel)
+        {
+            EmpProfile emp = new EmpProfile();
+            emp.EmpCode = empModel.EmpCode;
+            emp.EmpName = empModel.EmpName;
+            emp.DateOfBirth = empModel.DateOfBirth;
+            emp.Email = empModel.Email;
+            emp.DeptCode = empModel.DeptCode;
+            profileOperations.insertEmpProfile(emp);
+
+        }
+
+        public List<EmpProfileModel> GetAll()
+        {
+            List<EmpProfile> list = profileOperations.ListOfEmployee();
+            List<EmpProfileModel> empList = new List<EmpProfileModel>();
+            foreach (EmpProfile empModel in list)
+            {
+                EmpProfileModel emp = new EmpProfileModel();
+                emp.EmpCode = empModel.EmpCode;
+                emp.EmpName = empModel.EmpName;
+                emp.DateOfBirth = empModel.DateOfBirth;
+                emp.Email = empModel.Email;
+                emp.DeptCode = empModel.DeptCode;
+                empList.Add(emp);
+            }
+            return empList;
+        }
+        public  EmpProfileModel GetById(int empCode)
+        {
+            EmpProfile empModel = profileOperations.GetEmployeeById(empCode);
+
+            
+                EmpProfileModel emp = new EmpProfileModel();
+                emp.EmpCode = empModel.EmpCode;
+                emp.EmpName = empModel.EmpName;
+                emp.DateOfBirth = empModel.DateOfBirth;
+                emp.Email = empModel.Email;
+                emp.DeptCode = empModel.DeptCode;
+
+                return emp;
+           
+        }
+        public void Put(int empCode, EmpProfileModel empModel)
+        {
+            EmpProfile emp = profileOperations.GetEmployeeById(empCode);
+
+            
+                // Update the employee profile with the new data.
+                emp.EmpName = empModel.EmpName;
+                emp.DateOfBirth = empModel.DateOfBirth;
+                emp.Email = empModel.Email;
+                emp.DeptCode = empModel.DeptCode;
+
+                // Save the updated employee profile.
+                profileOperations.updateEmpProfile(emp);
+            
+           
+        }
+        public void Delete(int empCode)
+        {
+            EmpProfile emp = profileOperations.GetEmployeeById(empCode);
+
+            
+                profileOperations.deleteEmpProfile(empCode);
+            
+        }
+
+
+    }
+}
---
+++ BussinessLogicLayer/EmpProfileOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+
+namespace BussinessLogicLayer
+{
+    public class EmpProfileOperations
+    {
+        EmpProfileRepository empProfileRepository = new EmpProfileRepository(new EmsContext());
+
+        public void insertEmpProfile(EmpProfile empProfile)
+        {
+            empProfileRepository.Insert(empProfile);
+
+        }
+
+        public List<EmpProfile> ListOfEmployee()
+        {
+            return empProfileRepository.GetAll().ToList();
+
+        }
+        public EmpProfile GetEmployeeById(int empCode)
+        {
+            return empProfileRepository.GetByCode(empCode); // Assuming you have a method named Get in your repository to retrieve an employee by ID.
+        }
+
+        public void updateEmpProfile(EmpProfile empProfile)
+        {
+            empProfileRepository.Update(empProfile); // Assuming you have an Update method in your repository to update an employee profile.
+        }
+        public void deleteEmpProfile(int empCode)
+        {
+            empProfileRepository.Delete(empCode); // Assuming you have a Delete method in your repository to delete an employee profile.
+        }
+
+
+    }
+}
